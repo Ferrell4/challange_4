04658fd9a86858909fcff8c57730e01bd89d7639
Ferrell4 <32971621+Ferrell4@users.noreply.github.com>
2022-07-27 20:05:27 -0400

    Loading

---
+++ challange/Assets/questions.js
@@ -0,0 +1,59 @@
+// Creating an arran and passing the number, questions, options, and answers
+
+let questions = [
+    {
+        numb : 1,
+        question: "What does HTML stand for",
+        answer: "Hyper Text Markup Language",
+        options:[
+            "Hyper Text Preprocessor Language",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Text Multi Language",
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for",
+        answer: "Cascading Style Sheet",
+        options:[
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet",
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PhP stand for",
+        answer: "Hypertext Preprocessor",
+        options:[
+            "Hypertext Prepackaging",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hypertext Preprocessor",
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL",
+        answer: "Structured Query Language",
+        options:[
+            "Stylish Question Language",
+            "Stylish Query Language",
+            "Statement Question Language",
+            "Structured Query Language",
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for",
+        answer: "Extensible Markup Language",
+        options:[
+            "Extensible Markup Language",
+            "Excutable Multiple Language",
+            "Extra Multi-Program Language",
+            "Examine Multiple Language",
+        ]
+    }   
+];
